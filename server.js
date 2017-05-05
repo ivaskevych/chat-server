284c4428fb79441d7aef92616cb7b20f47d92701
Roman Ivaskevych <ivaskevych@users.noreply.github.com>
2017-05-05 23:12:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 const bodyParser = require('body-parser');
 // const router = express.Router();
 const router = require("./router");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const mongoose = require("mongoose");
-// mongoose.connect("mongodb://test_api:111111@ds029381.mlab.com:29381/react-chat_db");
-mongoose.connect("mongodb://127.0.0.1:27017/react-chat_db");
+mongoose.connect("mongodb://test_api:111111@ds029381.mlab.com:29381/react-chat_db");
+// mongoose.connect("mongodb://127.0.0.1:27017/react-chat_db");
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
